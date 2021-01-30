022a5650797359f746beb3a261444bd28d0fa244
Viklysx <vikbelec111@yandex.ru>
2021-01-31 01:59:37 +0300

    feat: working with the timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,16 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
     let modalState = {};
+    let deadline = '2021-12-31';
     changeModalState(modalState);
     modals();
     tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider','.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block')
     forms(modalState);
+    timer('.container1', deadline);
 })
